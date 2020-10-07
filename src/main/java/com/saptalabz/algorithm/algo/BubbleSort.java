775a8c2284f5d88b4dc9e516f790cf65a000faad
Rohan2596 <rohankadam662@gmail.com>
2020-10-07 16:31:13 +0530

    REFRACTED:Bubble Sort Algorithm of application.

--- src/main/java/com/saptalabz/algorithm/algo/BubbleSort.java
+++ src/main/java/com/saptalabz/algorithm/algo/BubbleSort.java
@@ -8,7 +8,8 @@ import java.util.ArrayList;
  * @DATE 06 October 2020
  * @Description
  * Bubble Sort is sorting algorithm that used the technique of adjacency techniques
- *
+ * We used i for rows and j for columns
+ * Exchange of columns values takes places by comparing current hed and next head.
  *
  * */
 
@@ -25,9 +26,8 @@ public class BubbleSort {
     public int[] sortedList() {
         int size = this.input.length;
         System.out.println(size);
-
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j <size-1 ; j++) {
+            for (int j = 0; j <size-i-1 ; j++) {
                 if(input[j]>input[j+1]){
                     int temp=input[j];
                     input[j]=input[j+1];
