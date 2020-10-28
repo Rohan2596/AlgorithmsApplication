026e4678b6e4c3638d6480b9b47d4aa0c3fc4a6d
Rohan2596 <rohankadam662@gmail.com>
2020-10-28 12:59:58 +0530

    ADDED:Merge Sort Algorithms logic for divide and conquer and merging.

--- src/main/java/com/saptalabz/algorithm/algo/MergeSort.java
+++ src/main/java/com/saptalabz/algorithm/algo/MergeSort.java
@@ -9,32 +9,70 @@ public class MergeSort {
     }
 
     public int[] sortedList() {
+           mergeSort(this.input,0,input.length-1);
+
+        for (int value : this.input) {
+            System.out.print(value + " ");
+
+        }
+        System.out.println("");
 
         return this.input;
     }
 
-    public void merge(int[] A, int[] B) {
-        int size_a = A.length;
-        int size_b = B.length;
-        int[] C = new int[size_a + size_b];
-        int i = 1, j = 1, k = 1;
+    public void mergeSort(int input[],int low,int high){
+        if(low<high){
+            int mid=(low+high)/2;
+            mergeSort(input,low,mid);
+            mergeSort(input,mid+1,high);
+            merge(input,low,mid,high);
+        }
+    }
 
-        //Comparing for two list with each other.
-        while (i <= size_a && j < size_b) {
-            if (A[i] < B[j]) {
-                C[k++] = A[i++];
+    public void merge(int[] input,int low,int mid,int high) {
+
+
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        //Creating two sub arrays left and right
+        int left[] = new int[n1];
+        int right[] = new int[n2];
+
+
+        //Injecting left and right array
+        for (int i = 0; i < n1; i++)
+            left[i] = input[low + i];
+        for (int j = 0; j < n2; j++)
+            right[j] = input[mid + 1 + j];
+
+        // Maintain current index of sub-arrays and main array
+        int i, j, k;
+        i = 0;
+        j = 0;
+        k = low;
+
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                input[k] = left[i];
+                i++;
             } else {
-                C[k++] = B[i++];
+                input[k] = right[j];
+                j++;
             }
+            k++;
         }
-        //Merging remaining list
-        for (; i <= size_a; i++) {
 
-            C[k++] = A[i];
+        while (i < n1) {
+            input[k] = left[i];
+            i++;
+            k++;
         }
-        for (; j <= size_b; j++) {
 
-            C[k++] = B[j];
+        while (j < n2) {
+            input[k] = right[j];
+            j++;
+            k++;
         }
     }
 }
